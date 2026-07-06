b8c69caef34ddc86326d0ea3cf05160d29d0c284
VIRTHS12 <187938754+VIRTHS12@users.noreply.github.com.>
2026-07-06 19:59:20 +0700

    fix all bug tired bossplss1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,26 +37,38 @@ export default function RootLayout() {
             font-style: normal;
           }
           @font-face {
-            font-family: 'FontAwesome';
+            font-family: 'font-awesome';
             src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
-            font-family: 'Ionicons';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/fonts/ionicons.ttf') format('truetype');
+            font-family: 'FontAwesome6_Solid';
+            src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/webfonts/fa-solid-900.ttf') format('truetype');
+            font-weight: 900;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: 'FontAwesome6_Regular';
+            src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/webfonts/fa-regular-400.ttf') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: 'ionicons';
+            src: url('https://cdn.jsdelivr.net/npm/ionicons@7.1.2/dist/ionicons/ionicons.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
             font-family: 'anticon';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/ant-design-icons/2.0.0/fonts/AntDesign.ttf') format('truetype');
+            src: url('https://cdn.jsdelivr.net/npm/ant-design-icons@2.0.0/fonts/AntDesign.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
-            font-family: 'MaterialIcons';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/MaterialIcons-Regular.ttf') format('truetype');
+            font-family: 'material';
+            src: url('https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/font/MaterialIcons-Regular.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
