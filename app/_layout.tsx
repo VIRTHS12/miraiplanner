4a3a4dd29aaf7878a1c14c98c9ceadfcfe178798
VIRTHS12 <187938754+VIRTHS12@users.noreply.github.com.>
2026-07-06 20:05:42 +0700

    fix all bug tired bossplss2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,49 +26,37 @@ export default function RootLayout() {
         style.textContent = `
           @font-face {
             font-family: 'material-community';
-            src: url('https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/fonts/materialdesignicons-webfont.ttf') format('truetype');
+            src: url('/assets/fonts/MaterialCommunityIcons.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
             font-family: 'feather';
-            src: url('https://cdn.jsdelivr.net/npm/feather-font@1.0.0/src/fonts/feather.ttf') format('truetype');
+            src: url('/assets/fonts/Feather.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
-            font-family: 'font-awesome';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf') format('truetype');
+            font-family: 'FontAwesome';
+            src: url('/assets/fonts/FontAwesome.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
             font-family: 'FontAwesome6_Solid';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/webfonts/fa-solid-900.ttf') format('truetype');
+            src: url('/assets/fonts/FontAwesome6_Solid.ttf') format('truetype');
             font-weight: 900;
             font-style: normal;
           }
           @font-face {
             font-family: 'FontAwesome6_Regular';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/webfonts/fa-regular-400.ttf') format('truetype');
+            src: url('/assets/fonts/FontAwesome6_Regular.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
           @font-face {
             font-family: 'ionicons';
-            src: url('https://cdn.jsdelivr.net/npm/ionicons@7.1.2/dist/ionicons/ionicons.ttf') format('truetype');
-            font-weight: 400;
-            font-style: normal;
-          }
-          @font-face {
-            font-family: 'anticon';
-            src: url('https://cdn.jsdelivr.net/npm/ant-design-icons@2.0.0/fonts/AntDesign.ttf') format('truetype');
-            font-weight: 400;
-            font-style: normal;
-          }
-          @font-face {
-            font-family: 'material';
-            src: url('https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/font/MaterialIcons-Regular.ttf') format('truetype');
+            src: url('/assets/fonts/Ionicons.ttf') format('truetype');
             font-weight: 400;
             font-style: normal;
           }
